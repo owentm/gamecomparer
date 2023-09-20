b2bb98a667a8c27f7946a3fe7b9ed6052dfd5c37
Owen Murphy <owentmurp@gmail.com>
2023-09-20 10:57:45 -0700

    Added the second search, initalized functions for future user input

--- website.py
+++ website.py
@@ -1,8 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
+import re
 
-url = 'https://www.pcgamer.com/baldurs-gate-3-review/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+import spans
+from bs4 import BeautifulSoup
+def gameOneSearch(search1):
+    return "search"
+def gameTwoSearch(search2):
+    return "search"
+
+url1 = 'https://www.pcgamer.com/baldurs-gate-3-review/'
+url2 = 'https://www.pcgamer.com/starfield-review/'
+
+gamepage1 = requests.get(url1)
+gamepage2 = requests.get(url2)
+soup = BeautifulSoup(gamepage1.text, 'html.parser')
+soup2 = BeautifulSoup(gamepage2.text, features='html.parser')
+
+
+spans = soup.find('span', {'class' : 'score score-long'})
+spans2 = soup2.find(name='span', attrs={'class' : 'score score-long'})
+
+
 print(soup.title)
-print(soup.find('span class = "score score-long"'))
+print(spans)
+print(spans2)
